a50c200778db766b53ab95aeca8c697284760ff0
desmonddibba <desmonddibba@gmail.com>
2025-04-22 17:00:12 +0200

    Created ResponseDTO model

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI(title="Desmonds Crawler")
 
 
+class ResponseDTO(BaseModel):
+    domain: str
+    pages: list[str]
 
 
-@app.get("/pages")
-def get_pages(target: str):
-    pass
+
+
+
+
+
+@app.get("/pages", response_model=ResponseDTO)
+def get_pages(target: str = Query(..., description="Enter start URL")) -> ResponseDTO:
+    return ResponseDTO(domain=target, pages=[])
